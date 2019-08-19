950792a7a1bc87b40ccd821c2c29d2550f45cbab
zhaomeihong <mhzhao_sh@163.com>
2019-08-19 16:17:24 +0800

    临时运行

--- src/main/java/com/example/SpringbootApplication.java
+++ src/main/java/com/example/SpringbootApplication.java
@@ -5,11 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 该注解指定项目为springboot，由此类当作程序入口，自动装配 web 依赖的环境
-**/
+ **/
 @SpringBootApplication
 public class SpringbootApplication {
 
-    public static void main(String[] args) throws Exception {
-    SpringApplication.run(SpringbootApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 }
--- src/main/java/com/example/littleDemo/ToolTest.java
+++ src/main/java/com/example/littleDemo/ToolTest.java
@@ -1,17 +1,8 @@
 package com.example.littleDemo;
 
-import com.example.tool.MD5;
-
 public class ToolTest {
-	
-	public static void main(String[] args) {
-		
-		//MD5加密
-		System.out.println(MD5.createPassword("123456")); //加密后：E10ADC3949BA59ABBE56E057F20F883E
-		
-		//核对密码
-		System.out.println(MD5.authenticatePassword("E10ADC3949BA59ABBE56E057F20F883E", "123456")); //核对密码
-	
-	}
 
+    public static void main(String[] args) {
+        System.out.println("2222");
+    }
 }
